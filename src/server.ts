381281a41daa8bbfd1ed07f668270c0f133d79be
Rafael Neves <nevesrafael.dev@gmail.com>
2024-07-12 22:58:38 -0300

    :sparkles: Feat confirm-participant route

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { createTrip } from "./routes/create-trip";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
 import cors from "@fastify/cors";
+import { confirmParticipants } from "./routes/confirm-participant";
 
 const app = fastify();
 
@@ -16,6 +17,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipants);
 
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("Server running in port: 3333 🔥🔥🔥");
