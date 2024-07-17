0a08d42491c66e41ee341eec0d150cd4d3aaa676
Rafael Neves <nevesrafael.dev@gmail.com>
2024-07-17 20:26:56 -0300

    :sparkles: Feat crate-invite route

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ import { createActivities } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
 
 const app = fastify();
 
@@ -26,6 +28,8 @@ app.register(createActivities);
 app.register(getActivities);
 app.register(createLink);
 app.register(getLinks);
+app.register(getParticipants);
+app.register(createInvite);
 
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("Server running in port: 3333 🔥🔥🔥");
